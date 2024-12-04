0246faff9a1a6ed53fb1f074c1fd4b1a8db7bd9d
Jen <evunti@gmail.com>
2024-12-04 11:50:12 -0500

    added code to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const aboutLink = document.getElementById("about-link");
+const contactLink = document.getElementById("contact-link");
+
+aboutLink.addEventListener("click", () => {
+  window.location.href = "/about";
+});
+added;
